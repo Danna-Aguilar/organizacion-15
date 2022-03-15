99a21be03811635f53e6bc3250fb6f5e0c7f9b9b
Danna-Aguilar <69810489+Danna-Aguilar@users.noreply.github.com>
2022-03-15 13:34:51 -0600

    Add files via upload

---
+++ Asteroide.py
@@ -0,0 +1,49 @@
+from OpenGL.GL import *
+from glew_wish import *
+import glfw
+import math
+from Modelo import *
+
+class Asteroide(Modelo):
+
+    def __init__(self, x, y, direccion, velocidad):
+        super().__init__(x,y,0.0, velocidad, direccion)
+
+    def actualizar(self, tiempo_delta):
+        cantidad_movimiento = self.velocidad * tiempo_delta
+        self.posicion_x = self.posicion_x + (
+            math.cos(self.direccion * math.pi / 180) *
+            cantidad_movimiento )
+        self.posicion_y = self.posicion_y + (
+            math.sin(self.direccion * math.pi / 180 ) * 
+            cantidad_movimiento )
+
+        if self.posicion_x > 1.05:
+            self.posicion_x = -1.0
+        if self.posicion_x < -1.05:
+            self.posicion_x = 1.0
+        if self.posicion_y > 1.05:
+            self.posicion_y = -1.0
+        if self.posicion_y < -1.05:
+            self.posicion_y = 1.0
+
+    def dibujar(self):
+        glPushMatrix()
+        glTranslatef(self.posicion_x, self.posicion_y, self.posicion_z)
+        glBegin(GL_QUADS)
+        glColor3f(0.4, 0.9, 0.21)
+        glVertex3f(-0.05,0.05,0.0)
+        glVertex3f(0.05,0.05,0.0)
+        glVertex3f(0.05,-0.05,0.0)
+        glVertex3f(-0.05,-0.05,0.0)
+        glEnd()
+        
+        glBegin(GL_LINE_LOOP)
+        glColor3f(0.0, 0.0, 0.0)
+        glVertex3f(-0.05,0.05,0.0)
+        glVertex3f(0.05,0.05,0.0)
+        glVertex3f(0.05,-0.05,0.0)
+        glVertex3f(-0.05,-0.05,0.0)
+        glEnd()
+
+        glPopMatrix()
---
+++ Bala.py
@@ -0,0 +1,43 @@
+from OpenGL.GL import *
+from glew_wish import *
+import glfw
+import math
+from Modelo import *
+
+class Bala(Modelo):
+    
+    disparando = False
+
+    def __init__(self):
+        super().__init__(velocidad=0.75)
+
+    def dibujar(self):
+        if self.disparando:
+            glPushMatrix()
+            glTranslatef(self.posicion_x, self.posicion_y, self.posicion_z)
+            glBegin(GL_QUADS)
+            glColor3f(0.0, 0.0, 0.0)
+            glVertex3f(-0.01,0.01,0.0)
+            glVertex3f(0.01,0.01,0.0)
+            glVertex3f(0.01,-0.01,0.0)
+            glVertex3f(-0.01,-0.01,0.0)
+            glEnd()
+            glPopMatrix()
+
+    def actualizar(self, tiempo_delta):
+        if self.disparando:
+            cantidad_movimiento = self.velocidad * tiempo_delta
+            self.posicion_x = self.posicion_x + (
+                math.cos(self.direccion * math.pi / 180.0) * cantidad_movimiento
+            )
+            self.posicion_y = self.posicion_y + (
+                math.sin(self.direccion * math.pi / 180.0) * cantidad_movimiento
+            )
+            #checar si está chocando contra enemigos
+            #(Eso hay que hacerlo, no está hecho)
+
+            #Checar si se salió de los límites:
+            if (self.posicion_x > 1 or self.posicion_x < -1 or 
+                self.posicion_y> 1 or self.posicion_y < -1):
+                self.disparando = False
+
